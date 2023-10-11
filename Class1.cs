7fd45fd01a0fc19b9e8992bfb6a77fdc5e020fe7
Utkarsh Navadiya <69842254+utk1310@users.noreply.github.com>
2023-10-11 12:38:04 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnitDemonstration;
+using NUnit;
+using NUnit.Framework;
+
+namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+        
+
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
